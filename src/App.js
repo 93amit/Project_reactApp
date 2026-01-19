27e36544456e844c468007377760c8edc60b0050
AMIT G <150007512+93amit@users.noreply.github.com>
2026-01-19 20:11:39 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi Manjeeta a quick message for you:
+          This is my React application!
+          "You're my sunshine," "You make my heart happy," "I'm so lucky to have you," or "Every moment with you is a treasure".
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
